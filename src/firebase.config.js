933230ff9545cf68f89dda6981d93665b9f9a277
wojtek35 <wojtek.kowcz35@gmail.com>
2022-09-06 16:02:55 +0200

    fixed firebase bug

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -12,4 +13,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore()
